bda65693139f8d9a82309236ff45e198e0a6797e
wilsonmike <michaelwilsonsj@hotmail.com>
2021-07-12 12:55:45 -0400

    added adobe fonts and bg

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -7,12 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <link rel="stylesheet" href="https://use.typekit.net/cgc6pch.css" />
     <?php wp_head(); ?>
   </head>
   <body>
     <header>
       <h1><?php bloginfo('name'); ?></h1>
-      <small><?php bloginfo('description'); ?></small>
+      <!-- <hr /> -->
+      <span><?php bloginfo('description'); ?></span>
     </header>
   </body>
 </html>
