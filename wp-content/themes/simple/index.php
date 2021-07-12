857e77c096e084224dfe93af75886d0683bdf41f
wilsonmike <michaelwilsonsj@hotmail.com>
2021-07-12 12:24:00 -0400

    added dynamic data for header element

--- wp-content/themes/simple/index.php
+++ wp-content/themes/simple/index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<html <?php language_attributes(); ?>
+  >
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php bloginfo('name'); ?></title>
-</head>
-<body>
-    
-</body>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <header>
+      <h1><?php bloginfo('name'); ?></h1>
+      <small><?php bloginfo('description'); ?></small>
+    </header>
+  </body>
 </html>
